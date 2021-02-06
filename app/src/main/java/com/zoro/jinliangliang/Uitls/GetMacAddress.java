7991aa6978b25bbdf43f3d42a43179ef65a7988a
金樑樑 <1244347482@qq.com>
2021-02-06 10:05:29 +0800

    20210206   新增设备deivceId获取方式

---
+++ app/src/main/java/com/zoro/jinliangliang/Uitls/GetMacAddress.java
@@ -0,0 +1,39 @@
+package com.zoro.jinliangliang.Uitls;
+
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+
+/**
+ * Created by Zoro
+ * Created on 2021/2/4
+ * description:
+ */
+public class GetMacAddress {
+//    public String getMacAddress() {
+//
+//        String macAddress = null ;
+//        WifiManager wifiManager =
+//                (WifiManager)MyApplication.getContext().getSystemService(Context.WIFI_SERVICE);
+//        WifiInfo info = ( null == wifiManager ? null : wifiManager.getConnectionInfo());
+//
+//        if (!wifiManager.isWifiEnabled())
+//        {
+//            //必须先打开，才能获取到MAC地址
+//            wifiManager.setWifiEnabled( true );
+//            wifiManager.setWifiEnabled( false );
+//        }
+//        if ( null != info) {
+//            macAddress = info.getMacAddress();
+//        }
+//        return macAddress;
+//    }
+
+//    <uses-
+//    permission android:name ="android.permission.ACCESS_WIFI_STATE"> </uses-permission >
+//<uses-
+//    permission android:name ="android.permission.CHANGE_WIFI_STATE"> </uses-permission >
+//<uses-
+//    permission android:name ="android.permission.WAKE_LOCK"> </uses-permission >
+
+}
